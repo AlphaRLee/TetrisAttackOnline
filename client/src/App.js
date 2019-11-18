1068d1e9a0ea826a4c7cc44531d1ff1be21e8cd2
R Lee <alpha.r.lee.dev@gmail.com>
2019-11-17 19:38:58 -0700

    Fixed git issue, client is now recognized as dir

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+	state = { game: {} };
+
+	componentDidMount() {
+		fetch("/game")
+			.then(res => res.json())
+			.then(game => this.setState({ game }));
+	}
+
+	render() {
+		const { game } = this.state;
+
+		return (
+			<div className="App">
+				<h1>{game.name}</h1>
+				<p>{game.description}</p>
+			</div>
+		);
+	}
+}
+
+export default App;
